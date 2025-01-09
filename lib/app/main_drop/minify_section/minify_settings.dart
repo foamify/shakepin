47a507df4fd3190d9df225cc7eac9d5c01b05703
damywise <25608913+damywise@users.noreply.github.com>
2025-01-09 09:10:15 +0700

    comment cli.setup()

--- lib/app/main_drop/minify_section/minify_settings.dart
+++ lib/app/main_drop/minify_section/minify_settings.dart
@@ -199,6 +199,50 @@ class _MinifySettingsState extends State<MinifySettings> {
                               : GlassButton(
                                   radius: 12,
                                   onTap: () async {
+                                    // await cli.setup(
+                                    //   onProgress: (step, progress) {
+                                    //     logger.log(
+                                    //         '[Cli.setup] $step: ${(progress * 100).toStringAsFixed(1)}%');
+                                    //   },
+                                    //   onError: (error) {
+                                    //     logger.log('[Cli.setup] $error');
+                                    //   },
+                                    //   onSuccess: () async {
+                                    //     logger
+                                    //         .log('[Cli.setup] Setup completed');
+
+                                    //     await logger
+                                    //         .log('Minify button pressed');
+                                    //     if (items().isEmpty) {
+                                    //       await logger.log(
+                                    //           'Minify button pressed but items list is empty');
+                                    //       await logger.log(
+                                    //           'Action aborted - no files to process');
+                                    //       return;
+                                    //     }
+                                    //     await logger.log(
+                                    //         'Minify button pressed with ${items().length} items');
+                                    //     await logger.log('Files breakdown:');
+                                    //     await logger.log(
+                                    //         '- Video files: ${items().videoPaths.length}');
+                                    //     await logger.log(
+                                    //         '- Image files: ${items().imagePaths.length}');
+                                    //     await logger.log('Selected settings:');
+                                    //     await logger.log(
+                                    //         '- Video quality: ${_videoQuality.name}');
+                                    //     await logger.log(
+                                    //         '- Video format: ${_videoFormat.name}');
+                                    //     await logger.log(
+                                    //         '- Image quality: ${_imageQuality.name}');
+                                    //     await logger.log(
+                                    //         '- Image format: ${_imageFormat.name}');
+                                    //     await logger.log(
+                                    //         'Starting minification process...');
+                                    //     minifyFiles();
+                                    //   },
+                                    // );
+                                    // return;
+
                                     await logger.log('Minify button pressed');
                                     if (items().isEmpty) {
                                       await logger.log(
