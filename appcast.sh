98ab24626a8d4401b95e8edc349c2858a7ab932d
damywise <25608913+damywise@users.noreply.github.com>
2025-02-06 09:02:24 +0700

    update appcast

--- appcast.sh
+++ appcast.sh
@@ -2,5 +2,5 @@ ditto -ck --keepParent dist/ShakePin.app dist/ShakePin.zip
 rm -rf dist/temp
 ditto dist/ShakePin.zip dist/temp/ShakePin.zip
 
-./generate_appcast dist/temp -o dist/skpn-appcast.xml
+./generate_appcast dist/temp -o skpn-appcast.xml
 rm -rf dist/temp
